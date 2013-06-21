b9fffee9897d44d7dd0ec39d13d92f5586922097
Brandon Meyer <bmeyerkc@gmail.com>
2013-06-21 00:53:10 -0500

    Creating MVC Project and adding JsonNetResult

---
+++ ExpectedTools.MVC/JsonNetResult.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace ExpectedTools.MVC
+{
+    public class JsonNetResult : ActionResult
+    {
+        public Encoding ContentEncoding { get; set; }
+        public string ContentType { get; set; }
+        public object Data { get; set; }
+
+        public JsonSerializerSettings SerializerSettings { get; set; }
+        public Formatting Formatting { get; set; }
+
+        public static JsonNetResult New(object data)
+        {
+            return new JsonNetResult
+            {
+                Data = data
+#if DEBUG
+,
+                Formatting = Formatting.Indented
+#endif
+            };
+
+        }
+
+        public JsonNetResult()
+        {
+            SerializerSettings = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Include };
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            HttpResponseBase response = context.HttpContext.Response;
+
+            response.ContentType = !string.IsNullOrEmpty(ContentType)
+                                       ? ContentType
+                                       : "application/json";
+
+            if (ContentEncoding != null)
+                response.ContentEncoding = ContentEncoding;
+
+            if (Data == null) return;
+
+            var writer = new JsonTextWriter(response.Output) { Formatting = Formatting };
+
+            var serializer = JsonSerializer.Create(SerializerSettings);
+            serializer.Serialize(writer, Data);
+
+            writer.Flush();
+        }
+    }
+}
